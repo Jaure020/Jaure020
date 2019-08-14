dbd852c82561a54a08ad2811178712c14a3e5e00
Jeff Jaureguy <jaure020@cougars.csusm.edu>
2019-08-13 22:06:33 -0700

    Revert and revisions to functional program

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+class Photo(models.Model):
+    File_name= models.CharField(max_length=100)
+    photos= models.ImageField(upload_to='images', null=True, blank=True)
+
+def __str__(self):
+    return self.File_name
+
+
+
+
